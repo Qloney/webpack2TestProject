86819f728fdd1e3187e75512c8757c490282a3d8
Igor Arkhipenko <qloney@mail.ru>
2017-06-30 18:44:42 +0300

    Getting Production Ready

--- src/router.js
+++ src/router.js
@@ -1,41 +1,41 @@
 import React from 'react';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import {Router, Route, IndexRoute, hashHistory} from 'react-router';
 
 import Home from './components/Home';
 import ArtistMain from './components/artists/ArtistMain';
 
 const componentRoutes = {
-    component: Home,
-    path: '/',
-    indexRoute: { component: ArtistMain },
-    clildRoutes: [
-        {
-            path: 'artists/new',
-            getComponent(location, cb) {
-                System.import('./components/artists/ArtistCreate')
-                    .then(module => cb(null, module.default));
-            }
-        },
-        {
-            path: 'artists/:id',
-            getComponent(location, cb) {
-                System.import('./components/artists/ArtistDetail')
-                    .then(module => cb(null, module.default));
-            }
-        },
-        {
-            path: 'artists/:id/edit',
-            getComponent(location, cb) {
-                System.import('./components/artists/ArtistEdit')
-                    .then(module => cb(null, module.default));
-            }
-        }
-    ]
+  component: Home,
+  path: '/',
+  indexRoute: {component: ArtistMain},
+  clildRoutes: [
+    {
+      path: 'artists/new',
+      getComponent(location, cb) {
+        System.import('./components/artists/ArtistCreate')
+          .then(module => cb(null, module.default));
+      }
+    },
+    {
+      path: 'artists/:id',
+      getComponent(location, cb) {
+        System.import('./components/artists/ArtistDetail')
+          .then(module => cb(null, module.default));
+      }
+    },
+    {
+      path: 'artists/:id/edit',
+      getComponent(location, cb) {
+        System.import('./components/artists/ArtistEdit')
+          .then(module => cb(null, module.default));
+      }
+    }
+  ]
 };
 
 const Routes = () => {
   return (
-    <Router history={hashHistory} routes={componentRoutes} />
+    <Router history={hashHistory} routes={componentRoutes}/>
   );
 };
 
